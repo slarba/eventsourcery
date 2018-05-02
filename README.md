aca33d714a2555d86f3a488012edfa5c8ce8a759
Marko Lauronen <marko.lauronen@solita.fi>
2018-05-02 18:30:57 +0300

    travis build status and README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+[![Build Status](https://travis-ci.org/slarba/eventsourcery.svg?branch=master)](https://travis-ci.org/slarba/eventsourcery)
+
+# Introduction
+
+Eventsourcery is a small utility library for event sourcing in Java. It includes a simple JDBC event store,
+event collection, replay and migration tools.
--- evso/src/test/java/org/mlt/eso/EventReplayTest.java
+++ evso/src/test/java/org/mlt/eso/EventReplayTest.java
@@ -42,8 +42,6 @@ public class EventReplayTest {
         JDBCEventStore eventStore = new JDBCEventStore(ds);
         eventStore.createSchema();
 
-        assertTrue(false);
-
         Events.registerEventType("AggregateExampleCreated", AggregateExampleCreated.class);
         Events.registerEventType("CountIncreased", CountIncreasedEvent.class);
         Events.registerEventType("NameSet", NameSetEvent.class);
